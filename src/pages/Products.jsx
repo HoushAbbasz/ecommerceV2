d70dc8272140819d1a7cbdede9bc108fa0ba39e8
HoushAbbasz <abbaszhoushmand@gmail.com>
2026-02-16 18:53:53 -0500

    updated files for deployment

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-const API_URL = 'http://car-part-kingdom-env.eba-3nmrxnty.us-east-1.elasticbeanstalk.com/api/products';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 function Products({ darkMode, onToggleDarkMode }) {
   // store products from backend
